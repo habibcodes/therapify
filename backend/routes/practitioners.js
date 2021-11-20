81684f689a3a2ac805b23d5d8d6d7059cd68c5b9
habibcodes <habib.codes@gmail.com>
2021-11-19 23:18:07 -0500

    minor bug fix

--- backend/routes/practitioners.js
+++ backend/routes/practitioners.js
@@ -4,15 +4,13 @@ const practitioners = express.Router();
 /* Access all practitioners */
 practitioners.get('/', function (req, res, next) {
   const practioners = [
-    { name: "Dr. Alpha", email: "alpha@doctor.com"}
-    { name: "Dr. Beta", email: "beta@doctor.com"}
-  ]
+    { name: 'Dr. Alpha', email: 'alpha@doctor.com' },
+    { name: 'Dr. Beta', email: 'beta@doctor.com' },
+  ];
   res.json({});
 });
 practitioners.get('/:practitioner_id', function (req, res, next) {
-  const practitioner = [
-    { name: "Dr.Alpha", email: "alpha@doctor.com"}
-  ]
+  const practitioner = [{ name: 'Dr.Alpha', email: 'alpha@doctor.com' }];
   res.json({});
 });
 
