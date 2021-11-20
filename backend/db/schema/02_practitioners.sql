5c47186e78090bd89dd916125b27f90c80877475
Ahmed Tarabia <ahmedtarabia@gmail.com>
2021-11-20 16:13:43 -0500

    modification to schema

--- backend/db/schema/02_practitioners.sql
+++ backend/db/schema/02_practitioners.sql
@@ -3,7 +3,7 @@ DROP TABLE IF EXISTS practitioners CASCADE;
 CREATE TABLE practitioners(
     id SERIAL PRIMARY KEY NOT NULL,
     user_id INTEGER REFERENCES users(id) ON DELETE CASCADE,
-    specialty
-    verified
-    available
+    specialty VARCHAR(255) NOT NULL,
+    verified BOOLEAN NOT NULL,
+    available BOOLEAN NOT NULL
 );
