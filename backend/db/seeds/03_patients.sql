ae7f3fada2c204dc20a6bdd09b6d09e9008f32a6
Ahmed Tarabia <ahmedtarabia@gmail.com>
2021-11-20 19:20:05 -0500

    fix entry in patients

--- backend/db/seeds/03_patients.sql
+++ backend/db/seeds/03_patients.sql
@@ -3,6 +3,6 @@ patients(user_id, age, disease)
 VALUES
     (1, 20, 'Depression'),
     (1, 30, 'PTSD'),
-    (2, 25, 'Anxiety'),
+    (2, 25, 'Bipolar'),
     (3, 23, 'Anger Management'),
     (4, 45, 'GAD');
