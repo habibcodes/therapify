8ddc724052b03acc00faff73b89d1efd49567ab2
Ahmed Tarabia <ahmedtarabia@gmail.com>
2021-11-21 13:20:14 -0500

    seeded more granular data to db / fix syntax error

--- backend/app.js
+++ backend/app.js
@@ -1,35 +1,35 @@
-const express = require('express');
-const path = require('path');
-const cookieParser = require('cookie-parser');
-const logger = require('morgan');
+const express = require("express");
+const path = require("path");
+const cookieParser = require("cookie-parser");
+const logger = require("morgan");
 // cors/---------
-const cors = require('cors');
+const cors = require("cors");
 // db Helper/---------
-const dbHelpers = require('./helpers/dbHelpers')(db);
+// const dbHelpers = require('./helpers/dbHelpers')(db);
 
 // import DB/---------
-const db = require('./db');
+const db = require("./db");
 
 const app = express();
 
 // Middleware/-----
 // use cors before your routes are set up/-----
 app.use(cors());
-app.use(logger('dev'));
+app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 
 // Routers/-------
-const patientsRouter = require('./routes/patients');
-const practitionersRouter = require('./routes/practitioners');
+const patientsRouter = require("./routes/patients");
+const practitionersRouter = require("./routes/practitioners");
 
-app.use('/api/patients', patientsRouter);
-app.use('/api/practitioners', practitionersRouter);
+app.use("/api/patients", patientsRouter);
+app.use("/api/practitioners", practitionersRouter);
 
-app.get('/api/authenticate');
-app.post('/api/login');
-app.post('/api/register');
+app.get("/api/authenticate");
+app.post("/api/login");
+app.post("/api/register");
 
 module.exports = app;
