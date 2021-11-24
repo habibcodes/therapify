e18bf8544c8596f6131790c27c0e08c0996b2254
habibcodes <habib.codes@gmail.com>
2021-11-24 17:30:17 -0500

    minor file name convention correction

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,39 +1,27 @@
-
 import './App.css';
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
-import Navbar from './components/navbar'
-import Landing from './components/landing'
-import Dashboard from './components/dashboard'
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Navbar from './components/navbar';
+import Landing from './components/landing';
+import Dashboard from './components/dashboard';
 import Background from './components/background';
-import PracDash from './components/Prac-dashboard'
+import PracDash from './components/Prac-dashboard';
 import { CssBaseline } from '@material-ui/core';
 import Login from './components/Login';
 
-
-
-
-
 function App() {
-  
   return (
-    
     <BrowserRouter>
-  
-    <CssBaseline />
-    <div>
-    
-
-      <Navbar />
-      <Background />
-      <Routes>
-      <Route path="/" element={<Landing />}/>
-      <Route path="/dashboard" element={<Dashboard />} />
-      <Route path="/pracdash" element={<PracDash/>} />
-      <Route path="/login" element={<Login/>} />
-
-      </Routes>
-    </div>
-  
+      <CssBaseline />
+      <div>
+        <Navbar />
+        <Background />
+        <Routes>
+          <Route path='/' element={<Landing />} />
+          <Route path='/dashboard' element={<Dashboard />} />
+          <Route path='/pracdash' element={<PracDash />} />
+          <Route path='/login' element={<Login />} />
+        </Routes>
+      </div>
     </BrowserRouter>
   );
 }
