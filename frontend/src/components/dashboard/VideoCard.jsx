0b8c8aac16fe10225614f737805ca1a0d84541a1
Nikolay Yozov <nikolayyozov@Nikolays-MacBook-Pro.local>
2021-11-23 19:49:59 -0500

    attempt to add video to card

--- frontend/src/components/dashboard/VideoCard.jsx
+++ frontend/src/components/dashboard/VideoCard.jsx
@@ -1,14 +1,61 @@
 import { Box } from "@mui/system"
+import VideoPlayer from '../VideoChat/VideoPlayer';
+import Sidebar from '../VideoChat/Sidebar';
+import Notifications from '../VideoChat/Notifications';
+import { Typography, AppBar } from '@material-ui/core';
+
+import { makeStyles } from '@material-ui/core/styles';
+// videoChat imports
+
+
+const useStyles = makeStyles((theme) => ({
+  appBar: {
+    borderRadius: 15,
+    margin: '30px 100px',
+    display: 'flex',
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center',
+    width: '600px',
+    border: '2px solid black',
+
+    [theme.breakpoints.down('xs')]: {
+      width: '90%',
+    },
+  },
+  image: {
+    marginLeft: '15px',
+  },
+  wrapper: {
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+    width: '100%',
+  },
+}));
 
 export default function Card() {
-
+ const classes = useStyles();
   return (
     <div>
         
     <Box className="clickbox" sx={{ bgcolor: 'background.paper' }}>
-    <div>
-      Video Chat goes here
+    <div className={classes.wrapper}>
+        {/* webRTC */}
+        <AppBar className={classes.appBar} position='static'>
+          <Typography variant='h2' align='center'>
+            VideoChat
+          </Typography>
+        </AppBar>
+        {/* vidPlayer */}
+        <VideoPlayer />
+        {/* Sidebar -->notifications */}
+        <Sidebar>
+          <Notifications />
+        </Sidebar>
+        {/* webRTC */}
       </div>
+      
      
       </Box>
       <div>
