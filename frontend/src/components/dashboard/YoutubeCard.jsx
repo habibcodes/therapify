0dc12c712b0393831d5f8708798abd0d99b4db15
habibcodes <habib.codes@gmail.com>
2021-11-27 20:18:13 -0500

    clear for merge

--- frontend/src/components/dashboard/YoutubeCard.jsx
+++ frontend/src/components/dashboard/YoutubeCard.jsx
@@ -1,17 +1,8 @@
-<<<<<<< HEAD
-import { Box } from "@mui/system";
-import { useState, useEffect } from "react";
-import { TextField } from "@material-ui/core";
-import axios from "axios";
-import SingleVid from "./SingleVid";
-import "./singlevid.css";
-=======
 import { Box } from '@mui/system';
 import { useState, useEffect } from 'react';
 import { TextField } from '@material-ui/core';
 import axios from 'axios';
 import SingleVid from './SingleVid';
->>>>>>> origin/master
 
 export default function Card() {
   const [search, setSearch] = useState('');
@@ -50,7 +41,7 @@ export default function Card() {
           </form>
         </div>
         <br />
-        <div className="filterVideos">{filteredVideos}</div>
+        <div className='filterVideos'>{filteredVideos}</div>
 
         <div></div>
       </Box>
