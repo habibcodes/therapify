99c9544590f57d69367fb83c6e3d6ce7861a3425
Ahmed Tarabia <ahmedtarabia@gmail.com>
2021-11-27 19:11:24 -0500

    toggle works

--- frontend/src/components/dashboard/PracCard.jsx
+++ frontend/src/components/dashboard/PracCard.jsx
@@ -1,14 +1,57 @@
-import { Box } from '@mui/system';
-import { getPractitioners } from './helpers';
-import { useState, useEffect } from 'react';
-import { TextField } from '@material-ui/core';
+import { Box } from "@mui/system";
+import { getPractitioners } from "./helpers";
+import { useState, useEffect } from "react";
+import { TextField } from "@material-ui/core";
+import { Grid } from "@material-ui/core";
+import * as React from "react";
+import { styled } from "@mui/material/styles";
+import Card from "@mui/material/Card";
+import CardHeader from "@mui/material/CardHeader";
+import CardContent from "@mui/material/CardContent";
+import CardActions from "@mui/material/CardActions";
+import Collapse from "@mui/material/Collapse";
+import Avatar from "@mui/material/Avatar";
+import IconButton from "@mui/material/IconButton";
+import Typography from "@mui/material/Typography";
+import { red } from "@mui/material/colors";
+import FavoriteIcon from "@mui/icons-material/Favorite";
+import ShareIcon from "@mui/icons-material/Share";
+import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
+import "./praccard.css";
 
-export default function Card() {
+export default function PractitionerCard() {
+  const [expanded, setExpanded] = React.useState(false);
   const [practitioners, setPractitioners] = useState([]);
-  const [searchTerm, setSearchTerm] = useState('');
+  const [searchTerm, setSearchTerm] = useState("");
   useEffect(() => {
     getPractitioners().then((practitioners) => setPractitioners(practitioners));
   }, []);
+
+  const ExpandMore = styled((props) => {
+    const { expand, ...other } = props;
+    return <IconButton {...other} />;
+  })(({ theme, expand }) => ({
+    transform: !expand ? "rotate(0deg)" : "rotate(180deg)",
+    marginLeft: "auto",
+    transition: theme.transitions.create("transform", {
+      duration: theme.transitions.duration.shortest,
+    }),
+  }));
+
+  const handleExpandClick = (index) => {
+    // alert(index);
+    console.log(index);
+    const practitionerObject = practitioners[index];
+    if (practitionerObject.expanded) {
+      practitionerObject.expanded = false;
+    } else {
+      practitionerObject.expanded = true;
+    }
+    const newPractitioners = practitioners;
+    newPractitioners[index] = practitionerObject;
+    setPractitioners([...newPractitioners]);
+    setExpanded(!expanded);
+  };
 
   function updateSearchTerm(event) {
     console.log(event.target.value);
@@ -16,7 +59,7 @@ export default function Card() {
   }
 
   const filteredPractitioners = practitioners.filter((practitioner) => {
-    if (searchTerm === '') return true;
+    if (searchTerm === "") return true;
     for (const key in practitioner) {
       if (
         practitioner[key]
@@ -32,30 +75,68 @@ export default function Card() {
 
   return (
     <div>
-      <Box className='clickbox' sx={{ bgcolor: 'background.paper' }}>
+      <Box className="clickbox" sx={{ bgcolor: "background.paper" }}>
+        {/* <Grid container spacing={2}> */}
         <div>
           <>
             <TextField
-              id='outlined-basic'
-              label='Search Practitioners'
+              id="outlined-basic"
+              label="Search Practitioners"
               onChange={updateSearchTerm}
-              variant='outlined'
+              variant="outlined"
             />
 
-            <h1 style={{ color: '#f0f0f0' }}>{searchTerm}</h1>
-            {filteredPractitioners.map((practitioner) => (
-              <p className='practitioner' style={{ color: '#f0f0f0' }}>
-                {practitioner.first_name} {practitioner.last_name}{' '}
-                <strong>
-                  <em>specialty:</em>
-                </strong>{' '}
-                {practitioner.specialty}
-              </p>
+            <h1 style={{ color: "#f0f0f0" }}>{searchTerm}</h1>
+
+            {filteredPractitioners.map((practitioner, index) => (
+              // <Grid item>
+              <Card className="practitioner" style={{ color: "#f0f0f0" }}>
+                <CardHeader
+                  avatar={
+                    <Avatar sx={{ bgcolor: red[500] }} aria-label="recipe" >
+                      {`${practitioner.first_name[0]}${practitioner.last_name[0]}`}
+                     
+                    </Avatar>
+                  }
+                  title={`${practitioner.first_name} ${practitioner.last_name}`}
+                  subheader={`specialty: ${practitioner.specialty}`}
+                />
+
+                <CardActions disableSpacing>
+                  <IconButton aria-label="add to favorites">
+                    <FavoriteIcon />
+                  </IconButton>
+                  <IconButton aria-label="share">
+                    <ShareIcon />
+                  </IconButton>
+                  <ExpandMore
+                    expand={practitioner.expanded}
+                    onClick={() => handleExpandClick(index)}
+                    aria-expanded={practitioner.expanded}
+                    aria-label="show more"
+                  >
+                    <ExpandMoreIcon />
+                  </ExpandMore>
+                </CardActions>
+
+                <Collapse
+                  in={practitioner.expanded}
+                  timeout="auto"
+                  unmountOnExit
+                >
+                  <CardContent>
+                    <Typography paragraph>{practitioner.first_name}</Typography>
+                    <Typography paragraph>
+                      Heat 1/2 cup of the broth in a pot until simmering, add
+                      saffron and set aside for 10 minutes.
+                    </Typography>
+                  </CardContent>
+                </Collapse>
+              </Card>
             ))}
           </>
         </div>
       </Box>
-      <div></div>
     </div>
   );
 }
--- frontend/src/components/dashboard/YoutubeCard.jsx
+++ frontend/src/components/dashboard/YoutubeCard.jsx
@@ -1,20 +1,12 @@
-<<<<<<< HEAD
 import { Box } from "@mui/system";
 import { useState, useEffect } from "react";
 import { TextField } from "@material-ui/core";
 import axios from "axios";
 import SingleVid from "./SingleVid";
 import "./singlevid.css";
-=======
-import { Box } from '@mui/system';
-import { useState, useEffect } from 'react';
-import { TextField } from '@material-ui/core';
-import axios from 'axios';
-import SingleVid from './SingleVid';
->>>>>>> origin/master
 
 export default function Card() {
-  const [search, setSearch] = useState('');
+  const [search, setSearch] = useState("");
   const [searchResults, setSearchResults] = useState([]);
   const onSearchChanged = (event) => {
     const _title = event.target.value;
@@ -23,10 +15,10 @@ export default function Card() {
   };
   const onSubmit = (event) => {
     event.preventDefault();
-    console.log('search value 11111----->', search);
+    console.log("search value 11111----->", search);
     axios.get(`/api/youtube`, { params: { search: search } }).then((res) => {
       setSearchResults(res.data.videos);
-      console.log('test---->', res.data.videos);
+      console.log("test---->", res.data.videos);
     });
   };
 
@@ -36,15 +28,15 @@ export default function Card() {
 
   return (
     <div>
-      <Box className='clickbox' sx={{ bgcolor: 'background.paper' }}>
+      <Box className="clickbox" sx={{ bgcolor: "background.paper" }}>
         <div>
           <form onSubmit={onSubmit}>
-            <div className='form-controls'>
+            <div className="form-controls">
               <TextField
-                id='outlined-basic'
-                label='Search Videos'
+                id="outlined-basic"
+                label="Search Videos"
                 onChange={onSearchChanged}
-                variant='outlined'
+                variant="outlined"
               />
             </div>
           </form>
