61f1b261556d7563217157edd10147fb08cef2f4
Ahmed Tarabia <ahmedtarabia@gmail.com>
2021-11-28 20:58:44 -0500

    youtube video size adjustment

--- frontend/src/components/dashboard/SingleVid.jsx
+++ frontend/src/components/dashboard/SingleVid.jsx
@@ -7,6 +7,8 @@ export default function SingleVid(props) {
       className="singleVideo"
       id="player"
       type="text/html"
+      width="480"
+      height="280"
       src={props.src}
       frameBorder="0"
     ></iframe>
