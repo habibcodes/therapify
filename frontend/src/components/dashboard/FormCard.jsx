563e3d7154b1eea10d774f90c19c0abca0cb1ae9
habibcodes <habib.codes@gmail.com>
2021-11-28 20:59:22 -0500

    fetch from master

--- frontend/src/components/dashboard/FormCard.jsx
+++ frontend/src/components/dashboard/FormCard.jsx
@@ -1,10 +1,15 @@
-import { Box } from "@mui/system"
-import { FormControl, TextField, FormHelperText, Typography } from '@mui/material'
-import { useState } from 'react'
+import { Box } from '@mui/system';
+import {
+  FormControl,
+  TextField,
+  FormHelperText,
+  Typography,
+  Stack,
+} from '@mui/material';
+import { useState } from 'react';
 import { styled } from '@mui/material/styles';
-import { purple } from "@mui/material/colors";
-import { Button } from "@material-ui/core";
-
+import { purple } from '@mui/material/colors';
+import { Button } from '@material-ui/core';
 
 import FormGroup from '@mui/material/FormGroup';
 import FormControlLabel from '@mui/material/FormControlLabel';
@@ -12,7 +17,6 @@ import FormControlLabel from '@mui/material/FormControlLabel';
 import Checkbox from '@mui/material/Checkbox';
 
 export default function Card() {
- 
   const ColorButton = styled(Button)(({ theme }) => ({
     color: theme.palette.getContrastText(purple[500]),
     backgroundColor: purple[500],
@@ -20,12 +24,14 @@ export default function Card() {
       backgroundColor: purple[700],
     },
   }));
-  
 
   const [state, setState] = useState({
     anxiety: false,
     depression: false,
     PTSD: false,
+    OCD: false,
+    socialAnxiety: false,
+    eatingDisorder: false,
   });
 
   const handleChange = (event) => {
@@ -33,66 +39,115 @@ export default function Card() {
       ...state,
       [event.target.name]: event.target.checked,
     });
-    console.log(state)
+    console.log(state);
   };
 
-  const { anxiety, depression, PTSD } = state;
-  
-
+  const {
+    anxiety,
+    depression,
+    PTSD,
+    OCD,
+    socialAnxiety,
+    eatingDisorder,
+  } = state;
 
   return (
     <div>
-        
-    <Box className="clickbox" sx={{ bgcolor: 'background.paper' }}>
-    
-      <form>
-      <FormControl sx={{ m: 3 }}>
-      <TextField id="standard-basic" label="Name" variant="standard" />
-      <br/>
-      <Typography>Reason for visit</Typography>
-        <FormHelperText>(Select all that apply)</FormHelperText>
-        <FormGroup>
-          <FormControlLabel
-            control={
-              <Checkbox checked={anxiety} onChange={handleChange} name="anxiety" />
-            }
-            label="Anxiety"
-          />
-          <FormControlLabel
-            control={
-              <Checkbox checked={depression} onChange={handleChange} name="depression" />
-            }
-            label="Depression"
-          />
-          <FormControlLabel
-            control={
-              <Checkbox checked={PTSD} onChange={handleChange} name="PTSD" />
-            }
-            label="PTSD"
-          />
-        </FormGroup>
-        <TextField
-          id="outlined-multiline-static"
-          label="Extra Info"
-          multiline
-          rows={4}
-          
-        />
-        <br/>
-        <ColorButton type="submit" variant="contained">Submit</ColorButton>
-
-      </FormControl>
-      
-      </form>
-     
-    </Box>
-   </div>
-     
-    
-    
-        
-    
-
-
-  )
+      <Box className='clickbox' sx={{ bgcolor: 'background.paper' }}>
+        <form>
+          <FormControl sx={{ m: 3 }}>
+            <TextField
+              id='standard-basic'
+              label='Full Name'
+              variant='standard'
+            />
+            <br />
+            <TextField
+              id='date'
+              label='DoB'
+              type='date'
+              defaultValue='1975-01-01'
+              sx={{ width: 220 }}
+              InputLabelProps={{
+                shrink: true,
+              }}
+            />
+            <br />
+            <Typography>Reason for visit</Typography>
+            <FormHelperText>(Select all that apply)</FormHelperText>
+            <FormGroup>
+              <FormControlLabel
+                control={
+                  <Checkbox
+                    checked={anxiety}
+                    onChange={handleChange}
+                    name='anxiety'
+                  />
+                }
+                label='Anxiety'
+              />
+              <FormControlLabel
+                control={
+                  <Checkbox
+                    checked={depression}
+                    onChange={handleChange}
+                    name='depression'
+                  />
+                }
+                label='Depression'
+              />
+              <FormControlLabel
+                control={
+                  <Checkbox
+                    checked={PTSD}
+                    onChange={handleChange}
+                    name='PTSD'
+                  />
+                }
+                label='PTSD'
+              />
+              <FormControlLabel
+                control={
+                  <Checkbox checked={OCD} onChange={handleChange} name='OCD' />
+                }
+                label='Obsessive Compulsive Disorder'
+              />
+              <FormControlLabel
+                control={
+                  <Checkbox
+                    checked={socialAnxiety}
+                    onChange={handleChange}
+                    name='socialAnxiety'
+                  />
+                }
+                label='Social Anxiety'
+              />
+              <FormControlLabel
+                control={
+                  <Checkbox
+                    checked={eatingDisorder}
+                    onChange={handleChange}
+                    name='eatingDisorder'
+                  />
+                }
+                label='Eating Disorder'
+              />
+            </FormGroup>
+            <br />
+            <TextField
+              id='outlined-multiline-static'
+              label='Additional Information'
+              multiline
+              placeholder='Please provide any additional information here'
+              rows={2}
+            />
+            <br />
+            <ColorButton type='submit' variant='contained'>
+              Submit
+            </ColorButton>
+          </FormControl>
+        </form>
+      </Box>
+    </div>
+  );
 }
