3095df60fd5533a0b7ca611f044b700e40fae3b4
Ahmed Tarabia <ahmedtarabia@gmail.com>
2021-11-28 21:57:26 -0500

    spacing between vids

--- frontend/src/components/dashboard/SingleVid.jsx
+++ frontend/src/components/dashboard/SingleVid.jsx
@@ -1,16 +1,16 @@
-import { Box } from "@mui/system";
-// import "./singlevid.css";
-
 export default function SingleVid(props) {
   return (
-    <iframe
-      className="singleVideo"
-      id="player"
-      type="text/html"
-      width="480"
-      height="280"
-      src={props.src}
-      frameBorder="0"
-    ></iframe>
+    <div>
+      <iframe
+        className="singleVideo"
+        id="player"
+        type="text/html"
+        width="480"
+        height="280"
+        src={props.src}
+        frameBorder="0"
+      ></iframe>
+      <br />
+    </div>
   );
 }
